61140625ebbfdf96f3b7f851a83d78906b9b4b29
NLuziaf <lategreentea835@gmail.com>
2022-05-28 15:34:07 +0700

    Adding more stuffs

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,3 +1,5 @@
+import random
+
 import discord
 from discord.ext import commands
 
@@ -22,6 +24,20 @@ class Fun(commands.Cog, command_attrs=dict(hidden=False)):
 
         await ctx.send(embed=embed)
 
+    @commands.hybrid_command(name="8ball")
+    async def eightball(self, ctx: commands.Context, question: str):
+        # LONGEST LIST OF RESPONSES, FEEL FREE TO ADD/REMOVE OR OPTIMISE
+        responses = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
+                    "Do not count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.",
+                    "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.",
+                    "Yes.", "Yes, definitely.", "You may rely on it."]
+        response = random.choice(responses)
+        
+        embed = discord.Embed(title="8ball is answering", description=f"{question}\nAnswer : {response}")
+        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url) # Support for nitro users
+        await ctx.send(embed=embed)
+
+    # NEXT : Lyrics command
     # DO YOUR COMMANDS HERE I HAVE NOT ENOUGH CREATIVITY TO THINK ABOUT THEM KEKW
 
 async def setup(bot):
