496d41d73425394235081a1313470ad1763fd3ea
NLuziaf <76501748+nluziaf@users.noreply.github.com>
2022-05-30 15:59:12 +0700

    some stuffs (#7)
    * binary
    
    * lyrics command
    
    Co-authored-by: SELECT stupidity FROM discord <102721047+SELECT-stupidity-FROM-discord@users.noreply.github.com>

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,4 +1,6 @@
 import random
+from urllib import parse
+from textwrap import wrap
 
 import discord
 from discord.ext import commands
@@ -7,8 +9,7 @@ from ext.models import CodingBot
 
 class Fun(commands.Cog, command_attrs=dict(hidden=False)):
 
-    hidden = False
-    
+    hidden = False    
     def __init__(self, bot: CodingBot) -> None:
         self.bot = bot
 
@@ -22,8 +23,8 @@ class Fun(commands.Cog, command_attrs=dict(hidden=False)):
         meme_poster = meme_json['author']
         meme_sub = meme_json['subreddit']
 
-        embed = discord.Embed(
-            title=meme_name, description=f"Meme by {meme_poster} from subreddit {meme_sub}")
+        embed = discord.Embed(title = meme_name, description=f"Meme by {meme_poster} from subreddit {meme_sub}")
+        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url)
         embed.set_image(url=meme_url)
 
         await ctx.send(embed=embed)
@@ -31,41 +32,34 @@ class Fun(commands.Cog, command_attrs=dict(hidden=False)):
     @commands.hybrid_command(name="8ball")
     async def eightball(self, ctx: commands.Context, *, question: str):
         responses = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
-                     "Do not count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.",
-                     "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.",
-                     "Yes.", "Yes, definitely.", "You may rely on it."]
+                    "Do not count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.",
+                    "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.",
+                    "Yes.", "Yes, definitely.", "You may rely on it."]
         response = random.choice(responses)
-
-        embed = discord.Embed(title="8ball is answering",
-                              description=f"{question}\nAnswer : {response}")
-        # Support for nitro users
-        embed.set_footer(
-            text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url)
+        
+        embed = discord.Embed(title="8ball is answering", description=f"{question}\nAnswer : {response}")
+        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url) # Support for nitro users
         await ctx.send(embed=embed)
 
     @commands.hybrid_command(name="token")
     async def token(self, ctx: commands.Context):
-        # If you like, you can use sr_api
-        response = await self.bot.session.get("https://some-random-api.ml/bottoken")
+        response = await self.bot.session.get("https://some-random-api.ml/bottoken") # If you like, you can use sr_api
         json = await response.json()
 
         bottoken = json['token']
 
-        embed = discord.Embed(
-            title="Ha ha ha, I grabbed your bot token.", description=bottoken)
-        embed.set_footer(
-            text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url)
+        embed = discord.Embed(title="Ha ha ha, I grabbed your bot token.", description=bottoken)
+        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url)
         await ctx.send(embed=embed)
 
     @commands.hybrid_command(name="animal")
     async def animal(self, ctx: commands.Context, animal: str = None):
-        options = ("dog", "cat", "panda", "fox", "red_panda",
-                   "koala", "bird", "raccoon", "kangaroo")
+        options = ("dog", "cat", "panda", "fox", "red_panda", "koala", "bird", "raccoon", "kangaroo")
         if (not animal) or (animal and animal not in options):
             animal = random.choice(options)
 
         response = await self.bot.session.get(f"https://some-random-api.ml/animal/{animal}")
-        if response.status in range(200, 300):
+        if response.status in range(200,300):
             json = await response.json()
 
             image = json["image"]
@@ -75,13 +69,78 @@ class Fun(commands.Cog, command_attrs=dict(hidden=False)):
             embed.set_image(url=image)
             embed.set_footer(text=fact)
         else:
-            embed = discord.Embed(
-                title="ERROR!",  description=f"Received a bad status code of {response.status}")
+            embed = discord.Embed(title="ERROR!",  description=f"Received a bad status code of {response.status}")
+            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url)
+        
+        await ctx.send(embed=embed)
+
+    @commands.get(invoke_without_command=True)
+    async def binary(self, ctx: commands.Context):
+        embed = discord.Embed(title="Binary command", description="Available methods: ```encode```, ```decode```")
+
+        await ctx.send(embed=embed)
+
+    @binary.hybrid_command(name="encode")
+    async def binary_encode(self, ctx, *, string: str):
+        response = await self.bot.session.get(f"https://some-random-api.ml/binary?encode={string}")
+        if response.status in range(200,300):
+            json = await response.json()
+
+            binary_string = json['binary']
+
+            embed = discord.Embed(title="Encoded to binary", description=binary_string)
+            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url)
+        else:
+            embed = discord.Embed(title="ERROR!",  description=f"Received a bad status code of {response.status}")
+            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url)
+
+        await ctx.send(embed=embed)
+
+    @binary.hybrid_command(name="decode")
+    async def binary_decode(self, ctx, binary: str):
+        response = await self.bot.session.get(f"https://some-random-api.ml/binary?decode={binary}")
+        if response.status in range(200,300):
+            json = await response.json()
+
+            string = json['text']
+
+            embed = discord.Embed(title="Encoded to binary", description=string)
+            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url)
+        else:
+            embed = discord.Embed(title="ERROR!",  description=f"Received a bad status code of {response.status}")
+            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url)
+            
+        await ctx.send(embed=embed)
+
+    @binary.hybrid_command(name="lyrics")
+    async def lyrics(self, ctx, *, query: str = None):
+        if not query:
+            embed = discord.Embed(title = "No search argument!", description=f"You must provide a search argument or I couldn't find the lyrics")
+            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url)
+        song = parse.quote(query)
+
+        response = await self.bot.session.get(f"https://some-random-api.ml/lyrics?title={song}")
+        if response.status in range(200, 300):
+            json = await response.json()
+            
+            lyrics = json['lyrics']
+            artist = json['author']
+            title = json['title']
+            thumbnail = json['thumbnail']['genius']
+
+            for chunk in wrap(lyrics, 4096, replace_whitespace = False):
+                embed = discord.Embed(title = f"{artist} - {title}", description = chunk)
+                embed.set_thumbnail(url=thumbnail)
+                embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url)
+
+        else:
+            embed = discord.Embed(title="ERROR!",  description=f"Received a bad status code of {response.status}")
+            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.display_avatar.url)
 
         await ctx.send(embed=embed)
 
     # DO YOUR COMMANDS HERE I HAVE NOT ENOUGH CREATIVITY TO THINK ABOUT THEM KEKW
 
-
 async def setup(bot):
     await bot.add_cog(Fun(bot))
+    
